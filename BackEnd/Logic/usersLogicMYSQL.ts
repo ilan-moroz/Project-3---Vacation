d441453484f2e321866d04a47b3eb78b83352f43
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-04-09 14:33:02 +0300

    createUsersTable

---
+++ BackEnd/Logic/usersLogicMYSQL.ts
@@ -0,0 +1,15 @@
+import dalMySQL from '../Utils/dalMySQL'
+
+const createUsersTable = () => {
+  const SQLcommand = `
+  CREATE TABLE vacation.users (
+    userKey INT NOT NULL AUTO_INCREMENT,
+    privateName VARCHAR(45) NOT NULL,
+    lastName VARCHAR(45) NOT NULL,
+    email VARCHAR(90) NOT NULL,
+    password VARCHAR(45) NOT NULL,
+    role VARCHAR(45) NOT NULL,
+    PRIMARY KEY (userKey));
+  `
+  dalMySQL.execute(SQLcommand)
+}
--- BackEnd/server.ts
+++ BackEnd/server.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser'
 import ErrorHandler from './MiddleWare/routeNotFound'
+import config from './Utils/Config'
 
 // Create Server
 const server = express()
@@ -19,5 +20,14 @@ server.use(express.json())
 //Parse the body as JSON
 server.use(bodyParser.json())
 
+// How to use routes
+// server.use('/api/v1/vacation/users')
+// server.use('/api/v1/vacation/')
+
 // Handle errors (route not found)
 server.use('*', ErrorHandler)
+
+// Start the server
+server.listen(config.WebPort, () => {
+  console.log(`listening on http://${config.mySQLhost}:${config.WebPort}`)
+})
