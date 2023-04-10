f11a67d6c398ab31d98e6882ed0922a439d9df74
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-04-10 16:03:15 +0300

    login register

--- FrontEnd/src/Components/Layout/MainLayout/MainLayout.tsx
+++ FrontEnd/src/Components/Layout/MainLayout/MainLayout.tsx
@@ -1,14 +1,15 @@
-import Login from '../../Pages/Login/Login'
-import MainRoute from '../../Routes/MainRoute/MainRoute'
+// import Login from '../../Pages/Login/Login'
+import Register from '../../Pages/Register/Register'
+// import MainRoute from '../../Routes/MainRoute/MainRoute'
 import Footer from '../Footer/Footer'
-import Header from '../Header/Header'
+// import Header from '../Header/Header'
 import './MainLayout.css'
 
 function MainLayout(): JSX.Element {
   return (
     <div className="MainLayout">
       <main>
-        <Login />
+        <Register />
       </main>
       <footer>
         <Footer />
