cb0c85ec6079322bd7dc4ab5e5976b650064b68e
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-04-16 18:26:48 +0300

    nav

--- FrontEnd/src/Components/Layout/NavBar/NavBar.tsx
+++ FrontEnd/src/Components/Layout/NavBar/NavBar.tsx
@@ -110,7 +110,6 @@ function ResponsiveAppBar() {
               </Button>
             ))}
           </Box>
-
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
