a9af410487605d5afa3ed04a7ab9ec1bc304319e
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-04-16 21:37:17 +0300

    bg img

--- FrontEnd/src/Components/Layout/Main/Main.tsx
+++ FrontEnd/src/Components/Layout/Main/Main.tsx
@@ -1,10 +1,13 @@
 import NavBar from '../NavBar/NavBar'
+import BackGround from '../BackGround/BackGround'
+
 import './Main.css'
 
 function Main(): JSX.Element {
   return (
     <div className="Main">
       <NavBar />
+      <BackGround/>
     </div>
   )
 }
