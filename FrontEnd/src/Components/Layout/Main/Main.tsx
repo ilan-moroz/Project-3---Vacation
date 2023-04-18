41d6f0d3ff23e2cc65cc839edb8e7bbfec00cd49
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-04-18 14:03:45 +0300

    card

--- FrontEnd/src/Components/Layout/Main/Main.tsx
+++ FrontEnd/src/Components/Layout/Main/Main.tsx
@@ -1,13 +1,12 @@
 import NavBar from '../NavBar/NavBar'
 import BackGround from '../BackGround/BackGround'
-
 import './Main.css'
 
 function Main(): JSX.Element {
   return (
     <div className="Main">
       <NavBar />
-      <BackGround/>
+      <BackGround />
     </div>
   )
 }
