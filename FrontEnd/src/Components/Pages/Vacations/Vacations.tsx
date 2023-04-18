5f71f9bbecae7fa16a95098cd61e1a8e97ce08d5
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-04-18 20:26:41 +0300

    addvactionmodel

--- FrontEnd/src/Components/Pages/Vacations/Vacations.tsx
+++ FrontEnd/src/Components/Pages/Vacations/Vacations.tsx
@@ -1,11 +1,13 @@
 import Card from '../../Layout/Card/Card'
 import NavBar from '../../Layout/NavBar/NavBar'
+import AddVacationModal from '../addVacationModal/addVacationModal'
 import './Vacations.css'
 
 function Vacations(): JSX.Element {
   return (
     <div className="Vacations">
       <NavBar />
+      <AddVacationModal />
       <Card />
     </div>
   )
