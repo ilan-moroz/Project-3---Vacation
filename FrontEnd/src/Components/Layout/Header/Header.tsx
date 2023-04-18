77f84108e54399e780e6a840bb8683c434e4dbb4
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-04-18 20:09:20 +0300

    navlink

--- FrontEnd/src/Components/Layout/Header/Header.tsx
+++ FrontEnd/src/Components/Layout/Header/Header.tsx
@@ -1,9 +1,12 @@
+import { NavLink } from 'react-router-dom'
 import './Header.css'
 
 function Header(): JSX.Element {
   return (
     <div className="Header">
-      <h1>]SHN|TZEL V@CATI*NS[</h1>
+      <NavLink to="/">
+        <h1>]SHN|TZEL V@CATI*NS[</h1>
+      </NavLink>
     </div>
   )
 }
