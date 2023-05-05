3e23dc4c28756656140c4dac4ffde09032fcc7d3
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-05-05 18:46:56 +0300

    NavLink

--- FrontEnd/src/Components/Pages/Register/Register.tsx
+++ FrontEnd/src/Components/Pages/Register/Register.tsx
@@ -9,7 +9,7 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
 import Typography from '@mui/material/Typography'
 import { createTheme, ThemeProvider } from '@mui/material/styles'
 import Header from '../../Layout/Header/Header'
-import { useNavigate, Link as RouterLink } from 'react-router-dom'
+import { useNavigate, NavLink } from 'react-router-dom'
 import './Register.css'
 import { User } from '../../Model/User'
 import axios from 'axios'
@@ -223,9 +223,9 @@ export default function Register() {
               >
                 Register
               </Button>
-              <RouterLink to="/login" className="button">
+              <NavLink to="/login" className="button">
                 {'Already have an account? Login now!'}
-              </RouterLink>
+              </NavLink>
               <Copyright sx={{ mt: 5 }} />
             </form>
           </Box>
