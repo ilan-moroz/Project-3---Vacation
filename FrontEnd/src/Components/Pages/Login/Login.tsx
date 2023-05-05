697b49c3777c843f58eb513406ece7306cc86eb0
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-05-05 18:34:11 +0300

    link

--- FrontEnd/src/Components/Pages/Login/Login.tsx
+++ FrontEnd/src/Components/Pages/Login/Login.tsx
@@ -11,7 +11,7 @@ import Typography from '@mui/material/Typography'
 import { createTheme, ThemeProvider } from '@mui/material/styles'
 import './Login.css'
 import Header from '../../Layout/Header/Header'
-import { Link as RouterLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { InputAdornment } from '@mui/material'
 import { Email, Password } from '@mui/icons-material'
 
@@ -153,9 +153,9 @@ export default function Login() {
               >
                 Sign In
               </Button>
-              <RouterLink to="/register" className="button">
+              <Link to="/register" className="button">
                 {"Don't have an account? Register now!"}
-              </RouterLink>
+              </Link>
               <Copyright sx={{ mt: 5 }} />
             </Box>
           </Box>
