4be3ef67012766b1d2448526860aab198e17d71d
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-05-16 21:28:18 +0300

    - log

--- FrontEnd/src/Components/Pages/Vacations/addVacationModal/addVacationModal.tsx
+++ FrontEnd/src/Components/Pages/Vacations/addVacationModal/addVacationModal.tsx
@@ -30,7 +30,6 @@ export default function AddVacationModal() {
   const [image, setImage] = useState("");
 
   function handleChange(event: any) {
-    console.log(event.target.files);
     setImage(URL.createObjectURL(event.target.files[0]));
   }
 
