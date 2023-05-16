12a6886900d8a3d52a212854600dcd7f91368b1b
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-05-16 17:51:41 +0300

    -

--- FrontEnd/src/Components/Pages/Vacations/Vacations.tsx
+++ FrontEnd/src/Components/Pages/Vacations/Vacations.tsx
@@ -1,27 +1,12 @@
 import Card from "./VacationCard/VacationCard";
 import AddVacationModal from "./addVacationModal/addVacationModal";
 import "./Vacations.css";
-import { useState } from "react";
-import { Vacation } from "../../Model/Vacation";
-import { vacation } from "../../Redux/VacationStore";
 
 function Vacations(): JSX.Element {
-  const [vacations, setVacations] = useState<Vacation[]>(
-    vacation.getState().vacations.vacations
-  );
-
-  vacation.subscribe(() => {
-    setVacations(vacation.getState().vacations.vacations);
-  });
   return (
     <div className="Vacations">
       <AddVacationModal />
       <Card />
-      <div>
-        {vacations.map((item) => {
-          return <h3 key={item.vacationDesc}>{item.price}</h3>;
-        })}
-      </div>
     </div>
   );
 }
