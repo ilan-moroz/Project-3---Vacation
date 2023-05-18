ca2377d66c15ae2981370887fde749860f130a36
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-05-18 13:35:49 +0300

    get data from database and save in redux in vaction

--- FrontEnd/src/Components/Layout/Main/Main.tsx
+++ FrontEnd/src/Components/Layout/Main/Main.tsx
@@ -1,23 +1,7 @@
 import { useEffect } from "react";
 import "./Main.css";
-import { vacation } from "../../Redux/VacationStore";
-import axios from "axios";
-import { allVacationsAction } from "../../Redux/VacationReducer";
 
 function Main(): JSX.Element {
-  useEffect(() => {
-    //redux
-    // vacation.subscribe(() => {
-    if (vacation.getState().vacations.vacations.length < 1) {
-      console.log("getting data from backend....");
-      axios
-        .get("http://localhost:8080/api/v1/vacation/vacations/allVacations")
-        .then((response) => {
-          vacation.dispatch(allVacationsAction(response.data));
-        });
-    }
-    // });
-  }, []);
   return <div className="Main"></div>;
 }
 
--- FrontEnd/src/Components/Pages/Vacations/Vacations.tsx
+++ FrontEnd/src/Components/Pages/Vacations/Vacations.tsx
@@ -1,8 +1,25 @@
 import Card from "./VacationCard/VacationCard";
 import AddVacationModal from "./addVacationModal/addVacationModal";
 import "./Vacations.css";
+import { useEffect } from "react";
+import { vacation } from "../../Redux/VacationStore";
+import axios from "axios";
+import { allVacationsAction } from "../../Redux/VacationReducer";
 
 function Vacations(): JSX.Element {
+  //get data from database and save in redux
+  useEffect(() => {
+    // vacation.subscribe(() => {
+    if (vacation.getState().vacations.vacations.length < 1) {
+      console.log("getting data from backend....");
+      axios
+        .get("http://localhost:8080/api/v1/vacation/vacations/allVacations")
+        .then((response) => {
+          vacation.dispatch(allVacationsAction(response.data));
+        });
+    }
+    // });
+  }, []);
   return (
     <div className="Vacations">
       <AddVacationModal />
