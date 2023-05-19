86f14d2c2028095e829dd444d6fb1d10773d8383
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-05-19 15:42:03 +0300

    Pagination

--- FrontEnd/src/Components/Pages/Vacations/Vacations.tsx
+++ FrontEnd/src/Components/Pages/Vacations/Vacations.tsx
@@ -5,10 +5,18 @@ import { useEffect, useState } from "react";
 import { vacation } from "../../../Redux/VacationStore";
 import axios from "axios";
 import { allVacationsAction } from "../../../Redux/VacationReducer";
+import Pagination from "@mui/material/Pagination";
 
 function Vacations(): JSX.Element {
   // re render the page after get data
   const [refresh, setRefresh] = useState(false);
+  // for Pagination
+  const [page, setPage] = useState(1);
+  const itemsPerPage = 8;
+  // handle change for Pagination
+  const handleChange = (event: any, value: any) => {
+    setPage(value);
+  };
 
   // get data from database and save in redux
   const fetchVacations = () => {
@@ -35,18 +43,33 @@ function Vacations(): JSX.Element {
       <AddVacationModal fetchVacations={fetchVacations} />
       {/* get all vacations from redux and display all the vacations*/}
       <div className="vacationCards">
-        {vacation.getState().vacations.vacations.map((item) => (
-          <Card
-            key={item["vacationDestiny"]}
-            vacationDestiny={item["vacationDestiny"]}
-            vacationDesc={item["vacationDesc"]}
-            vacationStart={item["vacationStart"]}
-            vacationEnd={item["vacationEnd"]}
-            price={item["price"]}
-            photoFile={`http://localhost:8080/${item["photoFile"]}`}
-          />
-        ))}
+        {vacation
+          .getState()
+          .vacations.vacations.slice(
+            (page - 1) * itemsPerPage,
+            page * itemsPerPage
+          )
+          .map((item) => (
+            <Card
+              key={item["vacationDestiny"]}
+              vacationDestiny={item["vacationDestiny"]}
+              vacationDesc={item["vacationDesc"]}
+              vacationStart={item["vacationStart"]}
+              vacationEnd={item["vacationEnd"]}
+              price={item["price"]}
+              photoFile={`http://localhost:8080/${item["photoFile"]}`}
+            />
+          ))}
       </div>
+      <Pagination
+        count={Math.ceil(
+          vacation.getState().vacations.vacations.length / itemsPerPage
+        )}
+        page={page}
+        onChange={handleChange}
+        color="primary"
+        className="Pagination"
+      />
     </div>
   );
 }
