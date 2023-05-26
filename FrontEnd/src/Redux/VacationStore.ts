ef0f472e863ed2a64aad0a8871333cd7b33389b4
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-05-26 14:05:36 +0300

    redux persist for user or admin login

--- FrontEnd/src/Redux/VacationStore.ts
+++ FrontEnd/src/Redux/VacationStore.ts
@@ -1,13 +1,30 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import { UserReducer } from "./UserReducer";
 import { VacationReducer } from "./VacationReducer";
 
-//choose all reducers....
-const reducers = { users: UserReducer, vacations: VacationReducer };
+const persistConfig = {
+  key: "root",
+  storage,
+};
 
-//combine reducers.
-export const vacation = configureStore({
+const persistedUserReducer = persistReducer(persistConfig, UserReducer);
+const persistedVacationReducer = persistReducer(persistConfig, VacationReducer);
+
+const reducers = {
+  users: persistedUserReducer,
+  vacations: persistedVacationReducer,
+};
+
+const vacation = configureStore({
   reducer: reducers,
-  middleware: (getDefaultMiddleWare) =>
-    getDefaultMiddleWare({ serializableCheck: false }), //do not look on serialization errors
+  middleware: getDefaultMiddleware({
+    serializableCheck: false,
+    thunk: true,
+  }),
 });
+
+let persistor = persistStore(vacation);
+
+export { vacation, persistor };
--- FrontEnd/src/index.tsx
+++ FrontEnd/src/index.tsx
@@ -1,17 +1,23 @@
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import reportWebVitals from './reportWebVitals'
-import MainLayout from './Components/Layout/MainLayout/MainLayout'
-import { BrowserRouter } from 'react-router-dom'
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import MainLayout from "./Components/Layout/MainLayout/MainLayout";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { vacation, persistor } from "./Redux/VacationStore";
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 root.render(
-  <BrowserRouter>
-    <MainLayout />
-  </BrowserRouter>,
-)
+  <Provider store={vacation}>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <MainLayout />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>
+);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+reportWebVitals();
