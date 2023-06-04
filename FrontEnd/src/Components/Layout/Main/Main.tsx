796a5b3c4b8eb1e3e558386f3f04bc9891b46429
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-06-04 19:40:15 +0300

    main page design

--- FrontEnd/src/Components/Layout/Main/Main.tsx
+++ FrontEnd/src/Components/Layout/Main/Main.tsx
@@ -1,7 +1,24 @@
+import { useSelector } from "react-redux";
 import "./Main.css";
+import { RootState } from "../../../Redux/VacationStore";
 
 function Main(): JSX.Element {
-  return <div className="Main"></div>;
+  const user = useSelector((state: RootState) => state.users.currentUser);
+  return (
+    <div className="Main">
+      {user == null && (
+        <div className="content-container">
+          <h1>Welcome to Shnitzel Vacations</h1>
+          <p>Your journey towards the perfect vacation starts here.</p>
+          <p>To unlock a world of unforgettable experiences,</p>
+          <p>
+            please <a href="/register">register</a> or{" "}
+            <a href="/login">login.</a>{" "}
+          </p>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Main;
