3675e05461652ddf70601864e71d1eaa77e50cad
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-06-08 19:23:22 +0300

    protect the routes

--- FrontEnd/src/Routes/MainRoute/MainRoute.tsx
+++ FrontEnd/src/Routes/MainRoute/MainRoute.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import "./MainRoute.css";
 import Login from "../../Components/Pages/Login/Login";
 import Register from "../../Components/Pages/Register/Register";
@@ -6,16 +6,37 @@ import Main from "../../Components/Layout/Main/Main";
 import Page404 from "../../Components/Pages/Page404/Page404";
 import Vacations from "../../Components/Pages/Vacations/Vacations";
 import VacationsReport from "../../Components/Pages/VacationsReport/VacationsReport";
+import { useSelector } from "react-redux";
+import { RootState } from "../../Redux/VacationStore";
 
 function MainRoute(): JSX.Element {
+  const role = useSelector((state: RootState) => state.users.role);
+
+  const UserOrRedirect = () => {
+    if (role === "user" || role === "admin") {
+      return <Vacations />;
+    } else {
+      // If user is not logged in, redirect to login
+      return <Navigate to="/login" replace />;
+    }
+  };
+
+  const AdminOrRedirect = () => {
+    if (role === "admin") {
+      return <VacationsReport />;
+    } else {
+      // If user is not logged in, redirect to login
+      return <Navigate to="/*" replace />;
+    }
+  };
   return (
     <div className="MainRoute">
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/vacations" element={<Vacations />} />
-        <Route path="/vacationreports" element={<VacationsReport />} />
+        <Route path="/vacations" element={<UserOrRedirect />} />
+        <Route path="/vacationreports" element={<AdminOrRedirect />} />
         <Route path="*" element={<Page404 />} />
       </Routes>
     </div>
