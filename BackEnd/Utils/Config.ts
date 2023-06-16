5e37458ade8048746ed51301147a57586a5c237f
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-06-16 14:00:48 +0300

    pretrier

--- BackEnd/Utils/Config.ts
+++ BackEnd/Utils/Config.ts
@@ -1,11 +1,11 @@
 // CONFIG FOR DATABASE
 class Config {
-  public WebPort = 8080
-  public mySQLhost = 'localhost'
-  public mySQLuser = 'root'
-  public mySQLpass = '12345678'
-  public mySQLdatabase = 'vacation'
+  public WebPort = 8080;
+  public mySQLhost = "localhost";
+  public mySQLuser = "root";
+  public mySQLpass = "12345678";
+  public mySQLdatabase = "vacation";
 }
 
-const config = new Config()
-export default config
+const config = new Config();
+export default config;
